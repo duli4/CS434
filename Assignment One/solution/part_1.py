b67e07d8b7cea733624024e32a92d866ce3a64cb
Trevor Swope <swopet@vm-cs434-1.engr.oregonstate.edu>
2018-04-09 22:08:05 -0700

    add 1.2

--- Assignment One/solution/part_1.py
+++ Assignment One/solution/part_1.py
@@ -1,18 +1,19 @@
 import numpy as np
 import scipy as sp
 
-def load_X_and_Y(filename,features,rows):
+def load_X_and_Y(filename,features,rows,dummy):
    f = open(filename,"r")
    all_data = f.read().splitlines()
    f.close()
-   X = np.zeros( (rows,features + 1) )
+   X = np.zeros( (rows,features + (1 if dummy else 0)) )
    Y = np.zeros( (rows,1) )
    for i in xrange(0,rows):
       data = all_data[i].split()
       for x in xrange(0,features):
 	 X[i][x] = float(data[x])
       Y[i][0] = float(data[features])
-      X[i][features] = 1 #dummy variable
+      if dummy:
+      	X[i][features] = 1 #dummy variable
    return (X,Y)
 
 def calculate_w(X,Y):
@@ -21,16 +22,35 @@ def calculate_w(X,Y):
    w = np.matmul(np.matmul(X_T_X_inv,X_T),Y)
    return w
 
+def compute_ASE(w,X,Y):
+   SSE = 0
+   num_features = len(w)
+   num_points = len(X[0])
+   #assume the lenght of X and Y is the same
+   for i in xrange(0,num_points):
+      guess = 0
+      for x in xrange(0,num_features):
+	 guess = guess + w[x][0]*X[i][x]
+      SE = (Y[i][0]-guess)**2
+      SSE = SSE + SE
+   ASE = SSE/num_points
+   return ASE
+   
+
 def main():
    #1.1: Load X and Y, calculate w and report it
-   (X,Y) = load_X_and_Y("../housing_train.txt",13,433)
+   (X,Y) = load_X_and_Y("../housing_train.txt",13,433,True)
    w = calculate_w(X,Y)
    print "w:"
    print w
 
    #1.2: Apply learned weight vector to training data and testing data, respectively, and compute and report their ASEs
-
-   #1.3: Remove the dummy variable from X, repeat 2 and 3
+   ASE = compute_ASE(w,X,Y)
+   print "ASE for training data:", ASE
+   (X_test,Y_test) = load_X_and_Y("../housing_test.txt",13,74,True)
+   ASE_test = compute_ASE(w,X_test,Y_test)
+   print "ASE for testing data:", ASE_test
+   #1.3: Remove the dummy variable from X, repeat 1 and 2
 
    #1.4: Modify the data by adding random features
 if __name__ == "__main__": main()

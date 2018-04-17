f4e616f59e83cd7748eda00fc7f88345b8856a1f
William Selbie <selbiew@oregonstate.edu>
2018-04-17 15:46:56 -0700

    Tweaks

--- Assignment Two/main.py
+++ Assignment Two/main.py
@@ -1,2 +1,15 @@
+import sys
+import time
+#import numpy as np
+
 def main():
-	print "Hello World"
+	parse_data()
+	
+def parse_data():
+	training_file = open("knn_train.csv", 'r')
+	test_file = open("knn_test.csv", 'r')
+	
+	start_time0 = time.clock()
+	content = [[float(item) for item in line.split(',')] for line in training_file.read().splitlines()]
+	
+if __name__ == "__main__": main()

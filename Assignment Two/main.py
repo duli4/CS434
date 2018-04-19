8e2d39fab96d29b11b9c1eacf2152ee383c42c72
Trevor Swope <swopet@vm-cs434-1.engr.oregonstate.edu>
2018-04-19 11:39:52 -0700

    add knn

--- Assignment Two/main.py
+++ Assignment Two/main.py
@@ -1,10 +1,16 @@
 import sys
 import time
-#import numpy as np
+import numpy as np
+import heapq
 
 def main():
 	(training_data,testing_data) = parse_data() if len(sys.argv) != 3 else parse_data(sys.argv[1],sys.argv[2])
 	normalize(training_data,testing_data)
+	count = 0
+	for i in xrange(0,len(training_data)):
+	   answer = knn(training_data[:i]+training_data[i+1:],training_data[i][1:],1)
+	   if (answer != int(training_data[i][0])): count = count + 1
+	print "leave-one-out cross-validation error:",count,"/",len(training_data),"=",count*100/len(training_data),"%"
 
 def parse_data(training_filename = "knn_train.csv", testing_filename = "knn_test.csv"):
 	training_file = open(training_filename, 'r')
@@ -31,5 +37,9 @@ def normalize(training_data,testing_data):
 	for point in testing_data:
 	   for i in xrange(1,len(point)):
 	      point[i] = (point[i]-mins[i-1])/(maxs[i-1]-mins[i-1])
-	
+
+#returns 1 or -1
+def knn(training_data,point,k):
+   return 1 if sum([points[1] for points in heapq.nsmallest(k,[[np.linalg.norm([data[i+1]-point[i] for i in xrange(0,len(point))]),data[0]] for data in training_data])]) > 0 else -1
+
 if __name__ == "__main__": main()

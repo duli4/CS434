2cd38ca36c3327c9e048bd0d99978502006b92d5
William Selbie <selbiew@oregonstate.edu>
2018-04-19 14:58:55 -0700

    Fixed Trevor's heaping pile of garbage that he calls code

--- Assignment Two/main.py
+++ Assignment Two/main.py
@@ -65,13 +65,13 @@ def run_knn(training_data,testing_data):
 		cv_count = 0
 		train_error_count = 0
 		for i in xrange(0,len(training_data)):
-	   		answer = knn(training_data[:i]+training_data[i+1:],training_data[i][1:],k)
-	   		if (answer != int(training_data[i][0])): cv_count = cv_count + 1
+			answer = knn(training_data[:i]+training_data[i+1:],training_data[i][1:],k)
+			if (answer != int(training_data[i][0])): cv_count = cv_count + 1
 			answer = knn(training_data,training_data[i][1:],k)
 			if (answer != int(training_data[i][0])): train_error_count = train_error_count + 1
 		test_error_count = 0
 		for i in xrange(0,len(testing_data)):
-		   	answer = knn(training_data,testing_data[i][1:],k)
+		  answer = knn(training_data,testing_data[i][1:],k)
 			if (answer != int(testing_data[i][0])): test_error_count = test_error_count + 1
 		
 		print "training error count:",train_error_count,"/",len(training_data),"=",float(train_error_count*100)/float(len(training_data)),"%"	      
@@ -83,7 +83,6 @@ def parse_data(training_filename = "knn_train.csv", testing_filename = "knn_test
 	training_file = open(training_filename, 'r')
 	test_file = open(testing_filename, 'r')
 	
-	start_time0 = time.clock()
 	training_data = [[float(item) for item in line.split(',')] for line in training_file.read().splitlines()]
 	testing_data = [[float(item) for item in line.split(',')] for line in test_file.read().splitlines()]
 	training_file.close()
@@ -103,7 +102,6 @@ def normalize(training_data,testing_data):
 	   for i in xrange(1,len(point)):
 	      point[i] = (point[i]-mins[i-1])/(maxs[i-1]-mins[i-1])
 
-#returns 1 or -1
 def knn(training_data,point,k):
    return 1 if sum([points[1] for points in heapq.nsmallest(k,[[np.linalg.norm([data[i+1]-point[i] for i in xrange(0,len(point))]),data[0]] for data in training_data])]) > 0 else -1
 

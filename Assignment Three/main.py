5fbeb6c3aa3211692b07abf5fd0e88459c6cbe4a
Trevor Swope <swopet@vm-cs434-1.engr.oregonstate.edu>
2018-05-15 18:51:55 -0700

    add OrderedDict

--- Assignment Three/main.py
+++ Assignment Three/main.py
@@ -6,6 +6,7 @@ import torch.nn.functional as F
 import torch.optim as optim
 from torchvision import datasets, transforms
 from torch.autograd import Variable
+from collections import OrderedDict
 
 import numpy as np
 import matplotlib as mpl

dcf65fdfe5c8169d8c90e9a94dc3e6a6e24c24c6
Trevor Swope <swopet@vm-cs434-1.engr.oregonstate.edu>
2018-05-21 17:05:59 -0700

    add images to PCA

--- Assignment 4/pca.py
+++ Assignment 4/pca.py
@@ -1,20 +1,26 @@
 import numpy as np
+from PIL import Image
 
 def main():
 	data = load_data("unsupervised.txt")
 	mm = data.mean(0)
+	Image.fromarray(np.reshape(mm,(28,28)).astype('uint8'),mode='L').save('mean.png')
 	cm = np.cov(data, rowvar=False)
-	
-	vals, vecs = np.linalg.eig(cm)
-	et = [(vals[i], vecs[i]) for i in xrange(len(vals))]
+	vals, vecs = np.linalg.eigh(cm)
+	et = sorted([(float(vals[i]), vecs[i]) for i in xrange(len(vals))],key = lambda item:item[0],reverse=True)
 	
 	#PCA - Part One
+	i = 0
+	for (val,vec) in et[:10]:
+	   max_val = max(vec)
+	   modified_vec = list(map(lambda item:item*255.0/max_val,vec))
+	   Image.fromarray(np.reshape(modified_vec,(28,28)).astype('uint8'),mode='L').save('eigen_'+str(i)+'.png')
+	   i = i + 1
 	print [et[i][0] for i in xrange(10)]
 	
 	
 def load_data(filepath):
-	raw_data = [line.split(',') for line in open(filepath, 'r').read().splitlines()]
-	return np.matrix(raw_data, dtype='int')
+	return np.array([[float(item) for item in line.split(',')] for line in open(filepath, 'r').read().splitlines()])
 	
 if __name__ == "__main__":
 	main()
